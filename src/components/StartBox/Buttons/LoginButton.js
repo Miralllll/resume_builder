9059fbee3e0245d0808c5b476b81ebac3c4d2654
Miralllll <55250021+Miralllll@users.noreply.github.com>
2022-09-01 02:55:29 +0400

    Login Validations
    Files changed a lot because of beautifier

--- src/components/StartBox/Buttons/LoginButton.js
+++ src/components/StartBox/Buttons/LoginButton.js
@@ -1,23 +1,35 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../../Button/button.css";
 import { Tooltip } from "@material-ui/core";
-import { Grid  } from '@material-ui/core';
+import { Grid } from "@material-ui/core";
 
-function StartButton({title, isAdditional}) {
-
-    return <Grid id={`button ${title}`} container justifyContent="center">
-        <div id={`button ${title}`} className={`mt-3 ${ isAdditional === true ? "col-12" : "mb-3 col-8"}`}>
-            <Grid id={`button ${title}`} container justifyContent="center">
-                {/* <Tooltip className={title}> */}
-                    <button id={`button ${title}`} type="button" style={({backgroundColor: `${ isAdditional === true ? "#707070" : ""}`})}
-                    className = {`mi-btn starter-button logo-title ${ isAdditional === true ? "" : "starter-next"}`}>
-                        <h4 id={`button ${title}`} type="button" className="logo-title">{title}</h4>
-                    </button>
-                {/* </Tooltip> */}
-            </Grid>
-        </div> 
+function StartButton({ title, isAdditional }) {
+  return (
+    <Grid container justifyContent="center">
+      <div
+        className={`mt-3 ${isAdditional === true ? "col-12" : "mb-3 col-8"}`}
+      >
+        <Grid id={`button ${title}`} container justifyContent="center">
+          {/* <Tooltip className={title}> */}
+          <button
+            id={`button ${title}`}
+            type="button"
+            style={{
+              backgroundColor: `${isAdditional === true ? "#707070" : ""}`,
+            }}
+            className={`mi-btn starter-button logo-title ${
+              isAdditional === true ? "" : "starter-next"
+            }`}
+          >
+            <h4 id={`button ${title}`} type="button" className="logo-title">
+              {title}
+            </h4>
+          </button>
+          {/* </Tooltip> */}
         </Grid>
-    ;
+      </div>
+    </Grid>
+  );
 }
 
 export default StartButton;
--- src/components/StartBox/StarterBoxCont.js
+++ src/components/StartBox/StarterBoxCont.js
@@ -1,81 +1,114 @@
 import React, { useState, useEffect, useRef } from "react";
 import SectionElement from "../SectionElement/SectionElement";
 import LoginButton from "./Buttons/LoginButton";
-import { Grid  } from '@material-ui/core';
-import './starter.css';
+import { Grid } from "@material-ui/core";
+import "./starter.css";
 
-const StarterBoxCont = ({sectionNumber, updateSectionNumber, sectionInfo, fields, size, section_label}) => {
-    const [sectionBox, updateSectionBox] = new useState(sectionInfo);
-    const [errors, updateErrors] = useState({email: '', password: ''});
+const StarterBoxCont = ({
+  sectionNumber,
+  updateSectionNumber,
+  sectionInfo,
+  fields,
+  size,
+  section_label,
+}) => {
+  const [sectionBox, updateSectionBox] = new useState(sectionInfo);
+  const [errors, updateErrors] = useState({ email: "", password: "" });
 
-    const handleChange = async (e) => {
-        if(e.target.id === "button Submit") {
-            if(sectionNumber === 1) {
-                var errs = errors;
-                errs.email = '';
-                errs.password = '';
-                updateErrors(errs);
-                const email = sectionBox.email;
-                const password = sectionBox.password;
-                try {
-                    const res = await fetch('http://localhost:3040/signup', {
-                        method: "POST",
-                        body: JSON.stringify({email: email, password: password}),
-                        headers: {'Content-Type': 'application/json'}
-                    });
-                    const data = await res.json();
-                    if(data.errors) {
-                        updateErrors(data.errors);
-                    }
-                } catch (err) {
-                    console.log(err);
-                }
-            }
-            
-        } else if(e.target.id.startsWith("button")) {
-            updateSectionNumber(sectionNumber === 1 ? 0 : 1);
+  const handleChange = async (e) => {
+    if (e.target.id === "button Submit") {
+      var errs = errors;
+      errs.email = "";
+      errs.password = "";
+      updateErrors(errs);
+      console.log(sectionNumber);
+      const email = sectionBox.email;
+      const password = sectionBox.password;
+      try {
+        var lowerCased = section_label.toLowerCase().replace(/ /g, "");
+        console.log(lowerCased);
+        const res = await fetch(`http://localhost:3040/${lowerCased}`, {
+          method: "POST",
+          body: JSON.stringify({ email: email, password: password }),
+          headers: { "Content-Type": "application/json" },
+        });
+        const data = await res.json();
+        console.log(data);
+        if (data.errors) {
+          updateErrors(data.errors);
         }
-    };;
+        console.log(errors);
+        if (data.user) {
+          // relocation on main page to login
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    } else if (e.target.id.startsWith("button")) {
+      updateSectionNumber(sectionNumber === 1 ? 0 : 1);
+    }
+  };
 
-    return (
-        <Grid container style={({backgroundColor: "transparent"})}>
-            <div className="container-second">
-            {/* <Grid container justifyContent="center" className="mb-1 container-second"><h2>{section_label}</h2></Grid> */}
-            <form onClick={handleChange}>
-            <div className="starter-form">
-                <div className=" mb-4 col-12"><Grid container justifyContent="center"><h2>{section_label}</h2></Grid></div>
+  return (
+    <Grid container style={{ backgroundColor: "transparent" }}>
+      <div className="container-second">
+        {console.log(errors)}
+        <form onClick={handleChange}>
+          <div className="starter-form">
+            <div key={section_label} className=" mb-4 col-12">
+              <Grid container justifyContent="center">
+                <h2>{section_label}</h2>
+              </Grid>
+            </div>
 
-                {(() => {
-                    const items = [];
-                    var i = 0;
-                    for(var sz = size; i < sz ; i++) {
-                        const innerItems = [];
-                        innerItems.push(<SectionElement
-                                                key={i}
-                                                index={i}
-                                                sectionBox={sectionBox} 
-                                                updateSectionBox={updateSectionBox}
-                                                field={fields[i]} 
-                                                isMainPage={"no"} 
-                                                starterNumber={sectionNumber}
-                                                updateSectionNumber={updateSectionNumber}
-                                                errors={errors}
-                                                updateErrors={updateErrors}
-                                                />);
-                        items.push( <div key={i} className="row">{innerItems}</div>);
-                    }
-                    items.push(<div key={i + 1} className="row"><LoginButton key={i + 1} isAdditional={false} title={"Submit"}></LoginButton></div>);
-                    return items;
-                })()}
-            </div>
-            <div key={1} className="row"><LoginButton key={1} isAdditional={true} title={section_label === "Login" ? "Sign Up": "Login"}></LoginButton></div>
-            </form>
-            </div>
-            
-        </Grid>
-    );
+            {(() => {
+              const items = [];
+              var i = 0;
+              for (var sz = size; i < sz; i++) {
+                const innerItems = [];
+                innerItems.push(
+                  <SectionElement
+                    key={i + " " + section_label}
+                    index={i}
+                    sectionBox={sectionBox}
+                    updateSectionBox={updateSectionBox}
+                    field={fields[i]}
+                    isMainPage={"no"}
+                    starterNumber={sectionNumber}
+                    updateSectionNumber={updateSectionNumber}
+                    errors={errors}
+                    updateErrors={updateErrors}
+                  />
+                );
+                items.push(
+                  <div key={i} className="row">
+                    {innerItems}
+                  </div>
+                );
+              }
+              items.push(
+                <div key={i + 1} className="row">
+                  <LoginButton
+                    key={i + 1}
+                    isAdditional={false}
+                    title={"Submit"}
+                  ></LoginButton>
+                </div>
+              );
+              return items;
+            })()}
+          </div>
+          <div key={1} className="row">
+            <LoginButton
+              key={1}
+              isAdditional={true}
+              title={section_label === "Login" ? "Sign Up" : "Login"}
+            ></LoginButton>
+          </div>
+        </form>
+      </div>
+    </Grid>
+  );
 };
-
-
 
 export default StarterBoxCont;
